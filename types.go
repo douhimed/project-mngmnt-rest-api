64f2c5e04c2b6791923df3a1894b4cbd0a9e4cbc
mdouhi <mdouhi@sqli.com>
2024-07-01 20:03:42 +0200

    refacto

--- types.go
+++ types.go
@@ -8,8 +8,8 @@ type ErrorResponse struct {
 
 type Task struct {
 	ID           int64     `json:"id"`
-	Name         string    `json: "name"`
-	Status       string    `json: "status"`
+	Name         string    `json:"name"`
+	Status       string    `json:"status"`
 	ProjectID    int64     `json:"projectID"`
 	AssignetToID int64     `json:"assignetTo"`
 	CreatedAt    time.Time `json:"createdAt"`
